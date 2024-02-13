e1cbe7af665fd295a4a6d7e7fabe6b480afd90a7
“Raqib <codewithraqib@gmail.com>
2024-02-13 03:52:40 -0300

    password generator

--- src/App.js
+++ src/App.js
@@ -4,151 +4,94 @@ import './App.css';
 function App() {
 
 
-  const [questions, setQuestions] = useState([
-    { id: 1, question: 'top run scoreer in cricket?', answer: [{ selected: false, name: "Sachin" }, { selected: false, name: "Dravid" }, { selected: false, name: "Lara" }, { selected: false, name: "Kohli" },], correct: 'Sachin', answered: false, correct_answer: false },
-    { id: 2, question: 'top wicket taker in cricket?', answer: [{ selected: false, name: "Warne" }, { selected: false, name: "Murali" }, { selected: false, name: "McGrath" }, { selected: false, name: "Kumble" },], correct: 'Warne', answered: false, correct_answer: false },
-    { id: 3, question: 'top run scoreer in cricket?', answer: [{ selected: false, name: "Sachin" }, { selected: false, name: "Dravid" }, { selected: false, name: "Lara" }, { selected: false, name: "Kohli" },], correct: 'Sachin', answered: false, correct_answer: false },
-    { id: 4, question: 'top wicket taker in cricket?', answer: [{ selected: false, name: "Warne" }, { selected: false, name: "Murali" }, { selected: false, name: "McGrath" }, { selected: false, name: "Kumble" },], correct: 'Warne', answered: false, correct_answer: false },
-    { id: 5, question: 'top run scoreer in cricket?', answer: [{ selected: false, name: "Sachin" }, { selected: false, name: "Dravid" }, { selected: false, name: "Lara" }, { selected: false, name: "Kohli" },], correct: 'Sachin', answered: false, correct_answer: false },
 
-  ]);
+  const STR = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const LENGTH = 8;
+
+  const [password, setPassowrd] = useState('kdhgfhgwe8698y67');
 
 
-  useEffect(() => {
+  // const checkLimit = (random, lowerLimit, upperLimit, double, lessThan) => {
 
-    fetch('https://the-trivia-api.com/v2/questions')
-      .then(res => res.json())
-      .then(data => {
+  //   if (!double) {
 
+  //     if (lessThan) {
+  //       if (random < lowerLimit) {
+  //         return true
+  //       }
+  //       return false
 
-        if (data?.length > 0) {
+  //     } else {
+  //       if (random > lowerLimit) {
+  //         return true
+  //       }
+  //       return false
+  //     }
 
-          let newQuestions = [...questions];
+  //   } else {
+  //     if (random > lowerLimit && random < upperLimit) {
+  //       return true;
+  //     }
+  //     return false;
+  //   }
+  // }
 
-          data.map((q, index) => {
+  const generatePassword = () => {
 
-            let newAnswers = [];
+    let passwordCreated = "";
 
-            q.incorrectAnswers.map((ans) => {
-              newAnswers.push({ name: ans, selected: false })
-            })
-            newAnswers.push({ name: q.correctAnswer, selected: false })
+    let crossedLimit0 = false;
+    let crossedLimit25 = false;
+    let crossedLimit51 = false;
 
-            newQuestions.push({ id: index + questions.length + 1, question: q.question["text"], answer: newAnswers, correct: q.correctAnswer, answered: false, correct_answer: false })
+    for (let i = 0; i < LENGTH; i++) {
+      let random = (Math.random() * 61).toFixed(0);
 
-          })
+      console.log(random);
 
-          setQuestions(newQuestions)
-        }
-        console.log(data)
-      })
-  }, [])
-
-
-
-
-
-  const chooseAnswer = (question, ans) => {
-    let newQuestions = []
-
-    questions.map(q => {
-      if (q.id === question.id) {
-
-        let newAnswers = [];
-
-        q.answer?.map(a => {
-          if (a.name === ans.name) {
-
-            newAnswers.push({ ...a, selected: true })
-          } else {
-            newAnswers.push({ ...a, selected: false })
-          }
-        })
+      // crossedLimit0 = checkLimit(random, 25, null, false, true);
+      // crossedLimit25 = checkLimit(random, 25, 51, true, null);
+      // crossedLimit51 = checkLimit(random, 51, null, false, false);
 
 
-        let correct_answer = false;
-
-
-        if (q.correct === ans.name) {
-          correct_answer = true
-        }
-
-        newQuestions.push({ ...q, answered: true, correct_answer: correct_answer, answer: newAnswers })
-      } else {
-        newQuestions.push(q)
+      if (random < 25) {
+        crossedLimit0 = true;
       }
-    })
 
-    setQuestions(newQuestions)
-
-  }
-
-
-  const getColor = (q) => {
-
-    let color = 'aliceblue';
-
-    if (q.answered) {
-      if (q.correct_answer) {
-        color = 'lightgreen'
-      } else {
-        color = 'lightcoral'
+      if (random > 25 && random < 51) {
+        crossedLimit25 = true;
       }
+
+      if (random > 51) {
+        crossedLimit51 = true;
+      }
+
+      passwordCreated += STR[random];
+
     }
 
-    return color;
+    // if (!crossedLimit0 || !crossedLimit25 || !crossedLimit51) {
+    //   generatePassword();
+    //   return;
+    // }
+    console.log(passwordCreated);
+
+    setPassowrd(passwordCreated);
+
   }
 
-
-  const getScore = () => {
-
-    let attempted = questions.filter(q => q.answered).length;
-
-    let total_questions = questions.length;
-
-    let correct_answers = questions.filter(q => q.correct_answer).length;
-
-
-    let percentage = (correct_answers / total_questions) * 100;
-
-
-    return (
-      <div>
-        <div>Attempted: {attempted}</div>
-        <div>Correct: {correct_answers}</div>
-        <div>Percentage: {percentage}%</div>
-        <div>{`${correct_answers}/${attempted}`}</div>
-      </div>
-    )
-  }
 
 
   return (
     <div className="App">
 
-      <div className="quiz_container">
 
-        <div>{getScore()}</div>
+      <button className='button' onClick={generatePassword}>Generate password</button>
 
-        {questions?.map(question => {
-          return (
-            <div className='question_ans_wrapper' style={{ backgroundColor: getColor(question) }}>
 
-              <div className='question'>{question.question}</div>
-              <div className='ans_wrapper'>
-                {question.answer?.map((ans, index) => {
-                  return <div className='ans' key={index}>
-                    <input disabled={question.answered} checked={ans.selected} type='radio' value={ans.name} onClick={() => chooseAnswer(question, ans)} />
-                    <span>{ans.name}</span>
-                  </div>
-                })}
-              </div>
+      <div className='password'> {password}</div>
 
-            </div>
-          )
-        })}
 
-      </div>
 
     </div>
   );
